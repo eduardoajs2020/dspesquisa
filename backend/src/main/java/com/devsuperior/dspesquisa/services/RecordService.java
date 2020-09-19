9d1d6481215a1ac544dfcea91c82a8896008a83f
eduardoajs2020 <eduardoajs2020@gmail.com>
2020-09-18 22:08:46 -0300

    GET records

--- backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
@@ -3,6 +3,8 @@ package com.devsuperior.dspesquisa.services;
 import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,7 +20,7 @@ public class RecordService {
 
 	@Autowired
 	private RecordRepository repository;
-	
+
 	@Autowired
 	private GameRepository gameRepository;
 
@@ -28,13 +30,19 @@ public class RecordService {
 		entity.setName(dto.getName());
 		entity.setAge(dto.getAge());
 		entity.setMoment(Instant.now());
-		
+
 		Game game = gameRepository.getOne(dto.getGameId());
 		entity.setGame(game);
-		
+
 		entity = repository.save(entity);
 		return new RecordDTO(entity);
 
 	}
 
+	@Transactional(readOnly = true)
+	public Page<RecordDTO> findByMoments(Instant minDate, Instant maxDate, PageRequest pageRequest) {
+		return repository.findByMoments(minDate, maxDate, pageRequest).map(x -> new RecordDTO(x));
+	}
+
+	
 }
